9c78c662dc5e3efd4b7261cf5250605aad35503a
Rafael <rafaelnd2001@gmail.com>
2024-04-04 23:34:11 -0300

    Adicionado entidade de produto servido e definido os tipos de todas as propriedades do sistema

---
+++ StudioHair.Core/Entities/ProdutosServico.cs
@@ -0,0 +1,18 @@
+﻿namespace StudioHair.Core.Entities
+{
+    public class ProdutosServico : Entidade
+    {
+        public ProdutosServico(int produtoId, int servicoId, int quantidadeUtilizada, decimal valorProdutoCalculado)
+        {
+            ProdutoId = produtoId;
+            ServicoId = servicoId;
+            QuantidadeUtilizada = quantidadeUtilizada;
+            ValorProdutoCalculado = valorProdutoCalculado;
+        }
+
+        public int ProdutoId { get; private set; }
+        public int ServicoId { get; private set; }
+        public int QuantidadeUtilizada { get; private set; }
+        public decimal ValorProdutoCalculado { get; private set; }
+    }
+}
