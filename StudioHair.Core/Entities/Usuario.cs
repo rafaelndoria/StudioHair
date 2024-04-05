525898184142955b7134b422d72232abf952a680
Rafael <rafaelnd2001@gmail.com>
2024-04-04 23:14:19 -0300

    Designado funcoes para criacao das entidades

--- StudioHair.Core/Entities/Usuario.cs
+++ StudioHair.Core/Entities/Usuario.cs
@@ -2,6 +2,14 @@
 {
     public class Usuario : Entidade
     {
+        public Usuario(string nome, string email, string papel, string senha)
+        {
+            Nome = nome;
+            Email = email;
+            Papel = papel;
+            Senha = senha;
+        }
+
         public string Nome { get; private set; }
         public string Email { get; private set; }
         public string Papel { get; private set; }
