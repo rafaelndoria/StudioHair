d2841c07f60f533e78cf9b22b508b0c1232f9405
Rafael <rafaelnd2001@gmail.com>
2024-04-04 23:11:25 -0300

    Adicionado entidade de usuario

---
+++ StudioHair.Core/Entities/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace StudioHair.Core.Entities
+{
+    public class Usuario : Entidade
+    {
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Papel { get; private set; }
+        public string Senha { get; private set; }
+    }
+}
