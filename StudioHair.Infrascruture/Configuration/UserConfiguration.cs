5f9cafbed79d5c2ba507fe61f5ee1c48698406b6
Rafael <rafaelnd2001@gmail.com>
2024-04-15 22:11:02 -0300

    Adicionado configuracao de usuario

---
+++ StudioHair.Infrascruture/Configuration/UserConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StudioHair.Core.Entities;
+
+namespace StudioHair.Infrascruture.Configuration
+{
+    public class UserConfiguration : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Nome)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(x => x.Email)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            builder.Property(x => x.Papel)
+                .IsRequired();
+
+            builder.Property(x => x.Senha)
+                .IsRequired()
+                .HasMaxLength(300);
+
+            builder.HasIndex(x => x.Email)
+                .IsUnique();
+
+            builder.HasIndex(x => x.Nome)
+                .IsUnique();
+        }
+    }
+}
---
+++ StudioHair.Infrascruture/Configuration/VendaConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StudioHair.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudioHair.Infrascruture.Configuration
+{
+    public class VendaConfiguration : IEntityTypeConfiguration<Venda>
+    {
+        public void Configure(EntityTypeBuilder<Venda> builder)
+        {
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
