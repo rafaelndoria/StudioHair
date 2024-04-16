ab047b5e670949eb815633f788ceb4f6b44e7dad
Rafael <rafaelnd2001@gmail.com>
2024-04-15 21:59:01 -0300

    Adicionado configuracao de contexto do entity freamework

---
+++ StudioHair.Infrascruture/Context/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudioHair.Core.Entities;
+
+namespace StudioHair.Infrascruture.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+
+        public DbSet<Agendamento> Agendamentos { get; set; }
+        public DbSet<AgendamentoServicos> AgendamentoServicos { get; set; }
+        public DbSet<Cliente> Cliente { get; set; }
+        public DbSet<HistoricoCliente> HistoricoClientes { get; set; }
+        public DbSet<HistoricoComprasClientes> HistoricoComprasClientes { get; set; }
+        public DbSet<Pessoa> Pessoa { get; set; }
+        public DbSet<Produto> Produto { get; set; }
+        public DbSet<ProdutosServico> ProdutosServicos { get; set; }
+        public DbSet<ProdutosVenda> ProdutosVendas { get; set; }
+        public DbSet<ProdutoUnidade> ProdutoUnidades { get; set; }
+        public DbSet<Servicos> Servicos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Venda> Vendas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
