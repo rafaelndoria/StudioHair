3e8daac484706e0809da5afaa1a93eee3bd332bf
Marcos <marcosjunior018@gmail.com>
2024-04-17 22:00:57 -0300

    Adicionado Configuration de Agendamento e Produto.

---
+++ StudioHair.Infrascruture/Configuration/AgendamentoConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using StudioHair.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudioHair.Infrascruture.Configuration
+{
+    public class AgendamentoConfiguration : IEntityTypeConfiguration<Agendamento>
+    {
+        public void Configure(EntityTypeBuilder<Agendamento> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Nome)
+                .IsRequired();
+
+            builder.Property(x => x.Dia)
+                .IsRequired();
+
+            builder.Property(x => x.HoraInicial)
+                 .IsRequired();
+
+            builder.Property(x => x.HoraFinal)
+                 .IsRequired();
+
+            builder.Property(x => x.ValorProfissional)
+                .IsRequired();
+
+            builder.Property(x => x.ValorAgendamento);
+
+            builder.Property(x => x.ValorDesconto);
+              
+            builder.Property(x => x.ClienteId)
+               .IsRequired();
+
+            builder.Property(x => x.Status)
+              .IsRequired();
+        }
+
+    }
+}
---
+++ StudioHair.Infrascruture/Configuration/ProdutoConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StudioHair.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudioHair.Infrascruture.Configuration
+{
+    public class ProdutoConfiguration : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Nome)
+                .IsRequired ();
+
+            builder.Property(x => x.Marca)
+                .IsRequired();
+
+            builder.Property(x => x.CodigoBarras)
+                 .IsRequired();
+
+            builder.Property(x => x.ValorPraticado)
+                 .IsRequired();
+
+            builder.Property(x => x.ProdutoParaVenda)
+                .IsRequired();
+
+            builder.Property(x => x.ControlaEstoque)
+                .IsRequired();
+
+            builder.Property(x => x.ControlaEstoque)
+               .IsRequired();
+        }
+    }
+}
